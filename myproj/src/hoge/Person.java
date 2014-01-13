40d9e29d50dceb3366df1160818ae5ab42a3d8d9
chiharu <chiharu.javamax@gmal.com>
2014-01-13 13:13:00 +0900

    myprojをEGitリポジトリーにコミット

---
+++ myproj/src/hoge/Person.java
@@ -0,0 +1,25 @@
+package hoge;
+
+public class Person {
+	private String name;
+	
+	private String motto;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMotto() {
+		return motto;
+	}
+
+	public void setMotto(String motto) {
+		this.motto = motto;
+	}
+	
+	
+}
